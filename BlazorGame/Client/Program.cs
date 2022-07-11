74e91e0f1709dee07aca8736c5a50e04ed50a3bb
bianyuwang <leoandlea@gmail.com>
2022-07-10 22:53:54 -0400

    Revert "be able to add unit"
    This reverts commit 3b8922a3f376fa6f7daf658715614973340f19a9.

--- BlazorGame/Client/Program.cs
+++ BlazorGame/Client/Program.cs
@@ -1,15 +1,11 @@
 using BlazorGame.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorGame.Client.Service;
-
-
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IUnitService, UnitService>();
+
 await builder.Build().RunAsync();
