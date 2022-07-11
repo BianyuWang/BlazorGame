2bff0a597d1a4f1309d972b3d6aafe09f501895e
bianyuwang <leoandlea@gmail.com>
2022-07-11 08:04:55 -0400

    try to add state management

---
+++ BlazorGame/Client/Service/IStateService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorGame.Client.Service
+{
+    public interface IStateService
+    {
+        public int TotalCoin { get; set; }
+
+        public event OnChange { get; set; }
+    }
+}
