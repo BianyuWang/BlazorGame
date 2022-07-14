7081a5385fe0798de46fc5f4873bb70c9c64600e
bianyuwang <leoandlea@gmail.com>
2022-07-14 18:22:02 -0400

    API Key backup

--- BlazorGame/Server/Service/NameGenerateService.cs
+++ BlazorGame/Server/Service/NameGenerateService.cs
@@ -26,14 +26,31 @@ namespace BlazorGame.Server.Service
                 Timeout = 1000
             };
             var client = new RestClient(options);
-
-            var request = new RestRequest($"?api_key={_Configuration["api_key"]}&endpoint={_Configuration["endpoint"]}&country_code={_Configuration["country_code"]}&results={_Configuration["results"]}");
-            var response = await client.ExecuteGetAsync(request);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+             RestRequest?  restRequest = null;
+            restRequest = new RestRequest($"?api_key={_Configuration["api_key_main"]}&endpoint={_Configuration["endpoint"]}&country_code={_Configuration["country_code"]}&results={_Configuration["results"]}");
+            RestResponse? response = null;
+            try
+            {
+                response = await client.ExecuteGetAsync(restRequest);
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    restRequest = new RestRequest($"?api_key={_Configuration["api_key_backup"]}&endpoint={_Configuration["endpoint"]}&country_code={_Configuration["country_code"]}&results={_Configuration["results"]}");
+                    response = await client.ExecuteGetAsync(restRequest);
+                }
+                catch (Exception ex)
+                { 
+                // need to do something here 
+                }
+            }
+            if (response != null&& response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 dynamic details = JsonConvert.DeserializeObject<ExpandoObject>(response.Content, new ExpandoObjectConverter());            
                 name = ((IEnumerable<dynamic>)details.data).First().name.firstname.name;                                     
             }
+           
 #pragma warning disable CS8603 // Possible null reference return.
             return name;
 #pragma warning restore CS8603 // Possible null reference return.
